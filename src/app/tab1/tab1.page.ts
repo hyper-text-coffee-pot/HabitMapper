766f4e20ab23db9eb407eea1a31924c08eaee4b5
Charles J <143773361+Hyper-Text-Coffee-Pot@users.noreply.github.com>
2025-03-21 18:46:34 -0500

    Form added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FirestoreService } from '../services/firestore.service';
+import { IonInput, IonItem, IonList } from '@ionic/angular/standalone';
 
 @Component({
 	selector: 'app-tab1',
 	templateUrl: 'tab1.page.html',
 	styleUrls: ['tab1.page.scss'],
-	standalone: false,
+	standalone: false
 })
 export class Tab1Page
 {
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,21 +10,6 @@ import { Router } from '@angular/router';
 export class Tab2Page
 {
 	constructor(
-		private afAuth: AngularFireAuth,
 		private router: Router
 	) { }
-
-	public async signOut(): Promise<void>
-	{
-		try
-		{
-			await this.afAuth.signOut();
-			console.log('User signed out');
-			localStorage.removeItem('user');
-			this.router.navigate(['/']);
-		} catch (error)
-		{
-			console.error('Error signing out:', error);
-		}
-	}
 }
